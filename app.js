cefffbe9f50220e51ba07d30b2cafe77629f4b42
pllearns <pllearns@gmail.com>
2017-06-13 17:35:25 -0700

    Setup contact form and nodemailer

--- app.js
+++ app.js
@@ -24,5 +24,33 @@ app.get('/contact', function(req, res) {
   res.render('contact')
 })
 
+app.post('/contact/send', function(req, res) {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'pllearns@gmail.com',
+      password: process.env.USER_PASSWORD
+    }
+  })
+
+  var mailOptions = {
+    from: 'Phillip Lorenzo <pllearns@gmail.com>',
+    to: 'phillip.lorenzo@gmail.com',
+    subject: 'Website Submission',
+    text: 'This is a simple message confirming submission to Name: '+req.body.name+'Email: '+req.body.email+'Message: '+req.body.message,
+    html: '<p>This is a simple message confirming submission</p><ul><li>Name: ' + req.body.name + '</li><li>Email: '+req.body.email+ '</li><li>Message: ' +req.body.message+ '</li></ul>'
+  }
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if(error) {
+      console.log(error)
+      res.redirect('/')
+    } else {
+      console.log('Message sent: ' + info.response)
+      res.redirect('/')
+    }
+  })
+})
+
 app.listen(3000)
 console.log('server is running on port 3000')
